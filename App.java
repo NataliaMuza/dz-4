b3038227caf09e5edd1e6990cf547b1b340cf271
Nataliia Myzuchenko <168213362+NataliaMuza@users.noreply.github.com>
2024-05-18 22:35:01 +0300

    Add method
    Добавила метод об'єкту, щоб вивести разом значення із завдання:
    
    1. Метод, який друкує в консоль "---<значення поля>---" і повертає його.
    2. У методі main створити екземпляр другого класу, викликати метод та вивести у консоль “~~~ <повернене значення> “~~~”.

--- App.java
+++ App.java
@@ -17,6 +17,7 @@ public class App
   //}
   {
       Automobile myAutomobile = new Automobile( "Kia", 2017);
+      System.out.printf(String.format("--- %s ---", myAutomobile.getModel()));
       System.out.printf(String.format("~~~ %s ~~~", myAutomobile.getYear()));
   }
 }
